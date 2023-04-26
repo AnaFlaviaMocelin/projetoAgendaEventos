3246d34e56c3ff67aabbac1802c19afcb6aa0f1d
ANA FLAVIA MOCELIN <81837573+AnaFlaviaMocelin@users.noreply.github.com>
2023-04-26 19:51:44 -0300

    Remoção
    Remoção para histórico de commits

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  getEnv(key, throwOnMissing, defaultValue) {
-    const value = process[key];
-    if (!value && throwOnMissing) {
-      throw new Error(`Missing ${key} on environment variables`);
-    }
-    return value ?? defaultValue;
-  },
-};
